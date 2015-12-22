604ad22b3828ba4fce6b8f0fc4d823da6c77af36
Riccardo Carlesso <palladiusbonton@gmail.com>
2015-12-22 15:43:33 +0100

    added 2 functions which I added from UI

--- index.js
+++ index.js
@@ -2,3 +2,14 @@ exports.helloworld = function(context, data) {
     console.log(data.message);
     context.success(data.message);
 };
+
+exports.onChangeRepoRicc = function(context, data) {
+    console.log("onChgRepo" + data.message);
+    context.success("onChgRepo" + data.message);
+};
+
+exports.provabucket = function(context, data) {
+    console.log("[provaBucket] "+data.message);
+    context.success("[provaBucket] "+data.message);
+};
+
